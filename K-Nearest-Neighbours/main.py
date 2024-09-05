aa20f722358c0a5fff25267d171e7af39e8285a2
Dawood Sarfraz <dawoodsarfraz.cs@gmail.com>
2024-09-05 09:59:31 +0500

    Update main.py

--- K-Nearest-Neighbours/main.py
+++ K-Nearest-Neighbours/main.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 
 class KNN:
@@ -6,4 +6,22 @@ class KNN:
         self.k = k
 
 
-   def  
+   def fit(self,X,y):
+       self.X_train = X
+       self.y_train = y
+
+
+    def predeict(self, X):
+        labels = [self._predicted(x) for x in X]
+        return np.array(labels)
+    
+    
+    def euclidean_distance(x,y):
+        return np.sqrt(np.sum((x-y)**2))
+
+    def _predict(self, x);
+        
+        distance = [euclidean_distance(x, x_train) for x_train in self.X]
+
+
+         
